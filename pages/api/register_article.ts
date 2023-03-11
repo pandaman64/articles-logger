e85fa2fe6b53662ea6b738b3d41bfe35fe1c372d
pandaman64 <kointosudesuyo@infoseek.jp>
2023-03-11 14:54:20 +0900

    Add DB insert API

---
+++ pages/api/register_article.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+import { z } from "zod";
+
+const prisma = new PrismaClient();
+
+const requestSchema = z.object({
+  title: z.string().nullable(),
+  text: z.string().nullable(),
+  url: z.string().nullable(),
+});
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const parseResult = requestSchema.safeParse(req.body);
+    if (parseResult.success) {
+      const data = parseResult.data;
+
+      const createResult = await prisma.article.create({
+        data,
+      });
+      console.log(createResult);
+
+      res.status(200).json({ status: "ok" });
+    } else {
+      res.status(400).json({ status: "failed" });
+    }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end();
+  }
+}
