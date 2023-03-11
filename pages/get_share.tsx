696657dd6c555bbef494484b01f694cffc149226
pandaman64 <kointosudesuyo@infoseek.jp>
2023-03-11 11:05:00 +0900

    Store articles in session storage

--- pages/get_share.tsx
+++ pages/get_share.tsx
@@ -1,17 +1,48 @@
+import Link from "next/link";
 import { useSearchParams } from "next/navigation";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+
+export type Article = {
+  title: string | null;
+  text: string | null;
+  url: string | null;
+};
 
 const GetShare: FC = () => {
   const searchParams = useSearchParams();
+  const title = searchParams.get("title");
+  const text = searchParams.get("text");
+  const url = searchParams.get("url");
+
+  const [isSet, setIsSet] = useState(false);
+  useEffect(() => {
+    if (title !== null || text !== null || url !== null) {
+      const articles: Article[] = JSON.parse(
+        sessionStorage.getItem("articles-logger-articles") ?? "[]"
+      );
+      articles.push({ title, text, url });
+      sessionStorage.setItem(
+        "articles-logger-articles",
+        JSON.stringify(articles)
+      );
+      console.log(articles);
+      setIsSet(true);
+    }
+  }, [title, text, url, setIsSet]);
 
   return (
-    <>
+    <div>
       <ul>
-        <li>title: {searchParams.get("title") ?? "null"}</li>
-        <li>text: {searchParams.get("text") ?? "null"}</li>
-        <li>url: {searchParams.get("url") ?? "null"}</li>
+        <li>title: {title ?? "null"}</li>
+        <li>text: {text ?? "null"}</li>
+        <li>url: {url ?? "null"}</li>
       </ul>
-    </>
+      {isSet && (
+        <div>
+          登録完了。<Link href="/">トップに戻る</Link>
+        </div>
+      )}
+    </div>
   );
 };
 
